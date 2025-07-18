1617aa0bbd85d1f9c8d7e17d31b7dbfefd44d513
zerofawks <abrarulhoque>
2025-07-18 16:48:42 +0600

    changed version

--- free-colour-samples.php
+++ free-colour-samples.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Free Colour Samples for WooCommerce
 Description: Adds a "Free Colour Sample" feature to WooCommerce products.
-Version: 2.1.0
+Version: 2.2.0
 Author: Expert Coders
 Text Domain: free-colour-samples
 Domain Path: /languages
